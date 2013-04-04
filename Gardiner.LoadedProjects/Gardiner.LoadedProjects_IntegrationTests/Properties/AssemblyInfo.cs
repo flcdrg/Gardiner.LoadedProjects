8f3dc257c98c313a674785d717616655a27111d3
David Gardiner <david@gardiner.net.au>
2013-04-04 23:51:16 +1030

    Bump version to 1.2

---
+++ Gardiner.LoadedProjects/Gardiner.LoadedProjects_IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gardiner.LoadedProjects_IntegrationTests.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MatrixGroup")]
+[assembly: AssemblyProduct("Gardiner.LoadedProjects_IntegrationTests.Properties")]
+[assembly: AssemblyCopyright("Copyright © MatrixGroup 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee3c59a6-8a27-4c38-b3e5-1b1629cee852")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Gardiner.LoadedProjects/Properties/AssemblyInfo.cs
+++ Gardiner.LoadedProjects/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Loaded Projects")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Visual Studio extension for managing loading/unloading projects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Gardiner")]
 [assembly: AssemblyProduct("Loaded Projects")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("David Gardiner 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: InternalsVisibleTo("Gardiner.LoadedProjects_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005b8764c4646f7e4748f2772dcdd90c19e19f19fd91982a6bea395b712ab99da83e3b530c0ad90fa49d8316cb8e4fb220456d42805943c3e441621398cb6b7451bd17bc8c8714e98d64f036a0f95ef6fbbdd8f776697f74e4cbae4cf555f7040ede6003ab0369108c39c5d077b15e5d6b5a7693804b1f0bfea0aae4af0a622ebf")]
 [assembly: InternalsVisibleTo("Gardiner.LoadedProjects_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005b8764c4646f7e4748f2772dcdd90c19e19f19fd91982a6bea395b712ab99da83e3b530c0ad90fa49d8316cb8e4fb220456d42805943c3e441621398cb6b7451bd17bc8c8714e98d64f036a0f95ef6fbbdd8f776697f74e4cbae4cf555f7040ede6003ab0369108c39c5d077b15e5d6b5a7693804b1f0bfea0aae4af0a622ebf")]
