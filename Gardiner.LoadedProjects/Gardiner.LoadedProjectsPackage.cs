978e832d5e295ffe7ee9343f96d12789f8588634
David Gardiner <david@gardiner.net.au>
2013-04-04 23:42:05 +1030

    Use DataContractSerializer
    Avoids problems with BinarySerializer not being able to find types

--- Gardiner.LoadedProjects/Gardiner.LoadedProjectsPackage.cs
+++ Gardiner.LoadedProjects/Gardiner.LoadedProjectsPackage.cs
@@ -5,7 +5,9 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 using EnvDTE;
@@ -27,16 +29,16 @@ namespace DavidGardiner.Gardiner_LoadedProjects
     /// </summary>
     // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
     // a package.
-    [PackageRegistration( UseManagedResourcesOnly = true )]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
     // This attribute is used to register the information needed to show this package
     // in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration( "#110", "#112", "1.0", IconResourceID = 400 )]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     // This attribute is needed to let the shell know that this package exposes some menus.
-    [ProvideMenuResource( "Menus.ctmenu", 1 )]
-    [Guid( GuidList.guidGardiner_LoadedProjectsPkgString )]
-// ReSharper disable InconsistentNaming
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(GuidList.guidGardiner_LoadedProjectsPkgString)]
+    // ReSharper disable InconsistentNaming
     public sealed class Gardiner_LoadedProjectsPackage : Package
-// ReSharper restore InconsistentNaming
+        // ReSharper restore InconsistentNaming
     {
         private const string SettingsKey = "Gardiner.LoadedProjects";
         private const string OutputWindowId = "C376C4E8-8E26-4D6F-886C-551A088EF57D";
@@ -54,8 +56,8 @@ namespace DavidGardiner.Gardiner_LoadedProjects
         /// </summary>
         public Gardiner_LoadedProjectsPackage()
         {
-            Debug.WriteLine( "Entering constructor for: {0}", ToString() );
-            AddOptionKey( SettingsKey );
+            Debug.WriteLine("Entering constructor for: {0}", ToString());
+            AddOptionKey(SettingsKey);
         }
 
 
@@ -68,18 +70,18 @@ namespace DavidGardiner.Gardiner_LoadedProjects
         /// </summary>
         protected override void Initialize()
         {
-            Debug.WriteLine( "Entering Initialize() of: {0}", ToString() );
+            Debug.WriteLine("Entering Initialize() of: {0}", ToString());
             base.Initialize();
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
-            var mcs = GetService( typeof (IMenuCommandService) ) as OleMenuCommandService;
-            if ( null != mcs )
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
             {
                 // Create the command for the menu item.
-                var menuCommandID = new CommandID( GuidList.guidGardiner_LoadedProjectsCmdSet,
-                                                   (int) PkgCmdIDList.cmdidLoadedProjects );
-                var menuItem = new MenuCommand( MenuItemCallback, menuCommandID );
-                mcs.AddCommand( menuItem );
+                var menuCommandID = new CommandID(GuidList.guidGardiner_LoadedProjectsCmdSet,
+                                                  (int) PkgCmdIDList.cmdidLoadedProjects);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
             }
 
             _settings = new Settings();
@@ -92,57 +94,57 @@ namespace DavidGardiner.Gardiner_LoadedProjects
         ///     See the Initialize method to see how the menu item is associated to this function using
         ///     the OleMenuCommandService service and the MenuCommand class.
         /// </summary>
-        private void MenuItemCallback( object sender, EventArgs e )
+        private void MenuItemCallback(object sender, EventArgs e)
         {
             GetProjects();
 
-            using ( var frm = new frmProfiles() )
+            using (var frm = new frmProfiles())
             {
-                frm.Unloaded = _unloaded.Select( x => x.HierarchyPath ).ToList();
+                frm.Unloaded = _unloaded.Select(x => x.HierarchyPath).ToList();
                 frm.Settings = _settings;
 
-                if ( frm.ShowDialog() != DialogResult.Cancel )
+                if (frm.ShowDialog() != DialogResult.Cancel)
                 {
-                    if ( frm.SelectedProfile != null )
+                    if (frm.SelectedProfile != null)
                     {
                         var profile = frm.SelectedProfile;
 
-                        IVsUIHierarchyWindow slnExpHierWin = VsShellUtilities.GetUIHierarchyWindow( this, VSConstants.StandardToolWindows.SolutionExplorer );
+                        IVsUIHierarchyWindow slnExpHierWin = VsShellUtilities.GetUIHierarchyWindow(this, VSConstants.StandardToolWindows.SolutionExplorer);
 
-                        var solutionService = (IVsSolution) GetService( typeof (SVsSolution) );
+                        var solutionService = (IVsSolution) GetService(typeof(SVsSolution));
 
-                        foreach ( var project in profile.UnloadedProjects )
+                        foreach (var project in profile.UnloadedProjects)
                         {
-                            var item = _loaded.FirstOrDefault( x => x.HierarchyPath == project );
+                            var item = _loaded.FirstOrDefault(x => x.HierarchyPath == project);
 
-                            if ( item != null )
+                            if (item != null)
                             {
-                                if ( slnExpHierWin == null )
+                                if (slnExpHierWin == null)
                                 {
-                                    Debug.Fail( "Failed to get the solution explorer hierarchy window!" );
+                                    Debug.Fail("Failed to get the solution explorer hierarchy window!");
                                 }
                                 else
                                 {
                                     ErrorHandler.ThrowOnFailure(
                                         solutionService.CloseSolutionElement(
-                                            (uint) __VSSLNCLOSEOPTIONS.SLNCLOSEOPT_UnloadProject, item.Hierarchy, 0 ) );
+                                            (uint) __VSSLNCLOSEOPTIONS.SLNCLOSEOPT_UnloadProject, item.Hierarchy, 0));
 
-                                    OutputCommandString( string.Format(CultureInfo.CurrentCulture, "Unloaded {0}", item.HierarchyPath ) );
+                                    OutputCommandString(string.Format(CultureInfo.CurrentCulture, "Unloaded {0}", item.HierarchyPath));
                                 }
                             }
                         }
 
-                        var dte = (DTE) GetService( typeof (DTE) );
+                        var dte = (DTE) GetService(typeof(DTE));
 
-                        foreach ( var pair in _unloaded )
+                        foreach (var pair in _unloaded)
                         {
-                            if ( profile.UnloadedProjects.All( x => x != pair.HierarchyPath ) )
+                            if (profile.UnloadedProjects.All(x => x != pair.HierarchyPath))
                             {
-                                ErrorHandler.ThrowOnFailure( slnExpHierWin.ExpandItem( pair.Hierarchy, (uint) VSConstants.VSITEMID.Root, EXPANDFLAGS.EXPF_SelectItem ) );
+                                ErrorHandler.ThrowOnFailure(slnExpHierWin.ExpandItem(pair.Hierarchy, (uint) VSConstants.VSITEMID.Root, EXPANDFLAGS.EXPF_SelectItem));
 
-                                dte.ExecuteCommand( "Project.ReloadProject" );
+                                dte.ExecuteCommand("Project.ReloadProject");
 
-                                OutputCommandString( string.Format(CultureInfo.CurrentCulture, "Reloaded {0}", pair.HierarchyPath ) );
+                                OutputCommandString(string.Format(CultureInfo.CurrentCulture, "Reloaded {0}", pair.HierarchyPath));
                             }
                         }
                     }
@@ -151,7 +153,7 @@ namespace DavidGardiner.Gardiner_LoadedProjects
         }
 
 
-        private static string GetFullPathToItem( IVsHierarchy hier )
+        private static string GetFullPathToItem(IVsHierarchy hier)
         {
             // Most hierarchies in the solution will QI to IVsProject, which allows us to use GetMkDocument to get the full path to the project file,
             // stub hierarchies (i.e. unloaded hierarchies) do not implement IVsProject, luckily their IVsHierarchy::GetCanonicalName returns the
@@ -160,17 +162,17 @@ namespace DavidGardiner.Gardiner_LoadedProjects
             var proj = hier as IVsProject;
 
             object prjObject;
-            if ( hier.GetProperty( 0xfffffffe, -2027, out prjObject ) >= 0 )
+            if (hier.GetProperty(0xfffffffe, -2027, out prjObject) >= 0)
             {
                 var p2 = (Project) prjObject;
 
-                if ( p2 != null && p2.Kind == ProjectKinds.vsProjectKindSolutionFolder )
+                if (p2 != null && p2.Kind == ProjectKinds.vsProjectKindSolutionFolder)
                     return string.Empty;
             }
 
 
             object pVar;
-            ErrorHandler.ThrowOnFailure(hier.GetProperty( (uint) VSConstants.VSITEMID.Root, (int) __VSHPROPID.VSHPROPID_Name, out pVar ));
+            ErrorHandler.ThrowOnFailure(hier.GetProperty((uint) VSConstants.VSITEMID.Root, (int) __VSHPROPID.VSHPROPID_Name, out pVar));
 
             int hr;
             if (proj != null)
@@ -194,17 +196,17 @@ namespace DavidGardiner.Gardiner_LoadedProjects
             _loaded = new List<HierarchyPathPair>();
             _unloaded = new List<HierarchyPathPair>();
 
-            var enumerator = new ProjectEnumerator( (IVsSolution) GetService( typeof (SVsSolution) ) );
+            var enumerator = new ProjectEnumerator((IVsSolution) GetService(typeof(SVsSolution)));
             var loadedProjects =
                 enumerator.LoadedProjects
-                .Select(x => new { hierarchy = (IVsUIHierarchy) x, path = GetFullPathToItem(x) })
-                .Where(x => x.path != null)
-                .Select(
-                    h => new HierarchyPathPair(  h.hierarchy, h.path ) );
+                          .Select(x => new {hierarchy = (IVsUIHierarchy) x, path = GetFullPathToItem(x)})
+                          .Where(x => x.path != null)
+                          .Select(
+                              h => new HierarchyPathPair(h.hierarchy, h.path));
 
             try
             {
-                loadedProjects.ForEach( hpp =>
+                loadedProjects.ForEach(hpp =>
                 {
                     if (_loaded != null)
                         _loaded.Add(hpp);
@@ -219,7 +221,7 @@ namespace DavidGardiner.Gardiner_LoadedProjects
                                       h => new HierarchyPathPair(h.hierarchy, h.path)));
 
 
-            unloadedProjects.ForEach( hpp => _unloaded.Add( hpp ) );
+                unloadedProjects.ForEach(hpp => _unloaded.Add(hpp));
             }
             catch (Exception e)
             {
@@ -229,78 +231,97 @@ namespace DavidGardiner.Gardiner_LoadedProjects
 
         private static void PrepareOutput()
         {
-            var outWindow = (IVsOutputWindow) GetGlobalService( typeof (SVsOutputWindow) );
+            var outWindow = (IVsOutputWindow) GetGlobalService(typeof(SVsOutputWindow));
 
-            var customGuid = new Guid( OutputWindowId );
+            var customGuid = new Guid(OutputWindowId);
             const string customTitle = "Loaded Projects Output";
-            ErrorHandler.ThrowOnFailure(outWindow.CreatePane( ref customGuid, customTitle, 1, 1 ));
+            ErrorHandler.ThrowOnFailure(outWindow.CreatePane(ref customGuid, customTitle, 1, 1));
 
-            ErrorHandler.ThrowOnFailure(outWindow.GetPane( ref customGuid, out _customPane ));
+            ErrorHandler.ThrowOnFailure(outWindow.GetPane(ref customGuid, out _customPane));
 
             ErrorHandler.ThrowOnFailure(_customPane.Activate()); // Brings this pane into view
         }
 
-        private static void OutputCommandString( string text )
+        private static void OutputCommandString(string text)
         {
-            ErrorHandler.ThrowOnFailure(_customPane.OutputString( text + "\n" ));
+            ErrorHandler.ThrowOnFailure(_customPane.OutputString(text + "\n"));
         }
 
-        public static Project ToDteProject( IVsHierarchy hierarchy )
+        public static Project ToDteProject(IVsHierarchy hierarchy)
         {
-            if ( hierarchy == null )
-                throw new ArgumentNullException( "hierarchy" );
+            if (hierarchy == null)
+                throw new ArgumentNullException("hierarchy");
             object prjObject;
-            if ( hierarchy.GetProperty( 0xfffffffe, -2027, out prjObject ) >= 0 )
+            if (hierarchy.GetProperty(0xfffffffe, -2027, out prjObject) >= 0)
             {
                 return (Project) prjObject;
             }
-            throw new ArgumentException( "Hierarchy is not a project." );
+            throw new ArgumentException("Hierarchy is not a project.");
         }
 
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes" )]
-        protected override void OnLoadOptions( string key, Stream stream )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        protected override void OnLoadOptions(string key, Stream stream)
         {
-            if ( key == SettingsKey )
+            if (key == SettingsKey)
             {
                 try
                 {
-                    var formatter = new BinaryFormatter();
-                    _settings = (Settings) formatter.Deserialize( stream );
+                    var formatter = new DataContractSerializer(typeof(Settings));
+                    _settings = (Settings) formatter.ReadObject(stream);
                 }
-                catch ( Exception )
+                catch (Exception ex)
                 {
+                    Debug.WriteLine("Exception loading options. {0}", ex);
                     _settings = new Settings();
                 }
             }
             else
             {
-                base.OnLoadOptions( key, stream );
+                base.OnLoadOptions(key, stream);
             }
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes" )]
-        protected override void OnSaveOptions( string key, Stream stream )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        protected override void OnSaveOptions(string key, Stream stream)
         {
-            if ( key == SettingsKey )
+            if (key == SettingsKey)
             {
                 try
                 {
-                    if ( null != _settings )
+                    if (null != _settings)
                     {
-                        var formatter = new BinaryFormatter();
-                        formatter.Serialize( stream, _settings );
+                        var formatter = new DataContractSerializer(typeof(Settings));
+                        formatter.WriteObject(stream, _settings);
                     }
                 }
-                catch ( Exception ex )
+                catch (Exception ex)
                 {
-                    Debug.WriteLine( ex );
+                    Debug.WriteLine("Exception saving options. {0}", ex);
                 }
             }
             else
             {
-                base.OnSaveOptions( key, stream );
+                base.OnSaveOptions(key, stream);
             }
         }
     }
+
+    internal sealed class AllowAllAssemblyVersionsDeserializationBinder : System.Runtime.Serialization.SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            String currentAssembly = Assembly.GetExecutingAssembly().FullName;
+
+            // In this case we are always using the current assembly
+            assemblyName = currentAssembly;
+
+            // Get the type using the typeName and assemblyName
+            Type typeToDeserialize = Type.GetType(String.Format("{0}, {1}", typeName, assemblyName));
+
+            return typeToDeserialize;
+        }
+    }
+
+
 }
